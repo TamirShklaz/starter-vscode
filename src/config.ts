198f322452f2be170c65e23cea958ea8736a5333
Anthony Fu <github@antfu.me>
2026-01-06 12:59:47 +0900

    chore: update deps

--- src/config.ts
+++ src/config.ts
@@ -1,7 +1,4 @@
-import { defineConfigObject } from 'reactive-vscode'
+import { defineConfig } from 'reactive-vscode'
 import * as Meta from './generated/meta'
 
-export const config = defineConfigObject<Meta.ScopedConfigKeyTypeMap>(
-  Meta.scopedConfigs.scope,
-  Meta.scopedConfigs.defaults,
-)
+export const config = defineConfig<Meta.ScopedConfigKeyTypeMap>(Meta.scopedConfigs.scope)
